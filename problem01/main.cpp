12f9e363c85937e9857c1dcdba58c796b5abe0af
Austin Meyer <meyerforge@gmail.com>
2020-10-04 15:08:41 -0700

    brute force complete, making small test.

--- problem01/main.cpp
+++ problem01/main.cpp
@@ -6,19 +6,59 @@
  *      Find the sum of all the multiples of 3 or 5 below 1000.
  */
 #include <vector>
+#include <iostream>
 
 
 using namespace std;
 
 
+vector<int> brute_multiples(int max);
+bool test_validiate(vector<int> results);
+
+
 int main(int argc, char** argv)
 {
+    vector<int> results = brute_multiples(1000);
 
+    for(unsigned int i = 0; i < results.size(); i++)
+    {
+        cout << results[i] << endl;
+    }
+
+    cout << "Size of results: " << results.size();
+}
+
+
+bool test_validiate(vector<int> results)
+{
+    for(unsigned int i = 0; i < results.size(); i++)
+    {
+        if(results[i] % 3 != 0 || results[i] % 5 != 0)
+        {
+            cout << "ERROR: Not a multiple of 3 or 5"
+        }
+    }
 }
 
 
 // Brute force approach
-int brute_multiples(int max)
+vector<int> brute_multiples(int max)
 {
+    vector<int> multiples_count = vector<int>();
 
+    multiples_count.push_back(3);
+
+    while(multiples_count.back() < 1000)
+    {
+        multiples_count.push_back(multiples_count.back() + 3);
+    }
+
+    multiples_count.push_back(5);
+
+    while(multiples_count.back() < 1000)
+    {
+        multiples_count.push_back(multiples_count.back() + 5);
+    }
+
+    return multiples_count;
 }
